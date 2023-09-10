a2f12c654dd763e4cc359f3c57dc5fd9fee071d2
zsiki <siki1958@gmail.com>
2023-09-10 16:33:42 +0200

    MTEXT added

--- python/text2csv.py
+++ python/text2csv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ create a csv file from dxf TEXT entites
-    position (x, y, z, alignment, rotation, layer and text are written to the output
+    x, y, z, alignment, rotation, layer and text are written to the output
 """
 
 import sys
@@ -8,8 +8,23 @@ import ezdxf
 import os.path
 
 def print_text(e, fo):
+    """ print data of an TEXT entity
+
+        :param e: entity to process
+        :param fo: output file to write to
+    """
     pos = e.get_pos()
     print(f'{pos[1][0]:.3f};{pos[1][1]:.3f};{pos[1][2]:.3f};{pos[0]};{e.dxf.rotation:.4f};{e.dxf.layer};{e.dxf.text}', file=fo)
+    
+def print_mtext(e, fo):
+    """ print data of an MTEXT entity, multiline texts are separated by '|'
+
+        :param e: entity to process
+        :param fo: output file to write to
+    """
+    # TODO aligment is always 0
+    pos = e.dxf.insert
+    print(f'{pos[0]:.3f};{pos[1]:.3f};{pos[2]:.3f};0;{e.dxf.rotation:.4f};{e.dxf.layer};{"|".join(e.plain_text(split=True))}', file=fo)
     
 if len(sys.argv) < 2:
     print(f'Usage: {sys.argv[0]} input_dxf [output_csv]')
@@ -37,3 +52,6 @@ msp = doc.modelspace()
 # entity query for all TEXT entities in modelspace
 for e in msp.query("TEXT"):
     print_text(e, fo)
+# entity query for all MTEXT entities in modelspace
+for e in msp.query("MTEXT"):
+    print_mtext(e, fo)
